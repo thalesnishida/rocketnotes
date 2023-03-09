19ac91938881ee8c61cf472f7cf21342136a0b1e
Thales Nishida <medeiros.tn@gmail.com>
2023-03-09 18:26:53 -0300

    add func handledAddLink

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 
-import { Textarea }  from '../../components/Textarea';
-import { NewItem }  from '../../components/NewItem';
-import { Section }  from '../../components/Section';
-import { Button }  from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Input }  from '../../components/Input';
+import { useState } from "react";
 
-import { Container, Form } from './styles';
+import { Textarea } from "../../components/Textarea";
+import { NewItem } from "../../components/NewItem";
+import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 
-export function New(){
+import { Container, Form } from "./styles";
+
+export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  function handleAddLink() {
+    setLinks((prevState) => [...prevState, newLink]);
+    setNewLink("");
+  }
+
   return (
     <Container>
       <Header />
@@ -21,25 +31,33 @@ export function New(){
             <Link to="/"> Voltar</Link>
           </header>
 
-          <Input placeholder="Título"/>
+          <Input placeholder="Título" />
           <Textarea placeholder="Observações" />
 
-
           <Section title="Links uteis">
-            <NewItem value="https://github.com/thalesnishida"/>
-            <NewItem isNew placeholder="Novo link"/>
+            {links.map((link, index) => (
+              <NewItem key={String(index)} value={link} onClick={() => {}} />
+            ))}
+
+            <NewItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={(e) => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
-            <div className='tags'>
-              <NewItem value="react"/>
-              <NewItem isNew placeholder="node"/>
+            <div className="tags">
+              <NewItem value="react" />
+              <NewItem isNew placeholder="node" />
             </div>
           </Section>
 
-          <Button title="Salvar"/>
-          </Form>
-        </main>
+          <Button title="Salvar" />
+        </Form>
+      </main>
     </Container>
-  )
+  );
 }
