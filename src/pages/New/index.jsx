150d580aa8bd9daf7b2ad876e7b971bbb85f2070
Thales Nishida <medeiros.tn@gmail.com>
2023-03-09 22:12:19 -0300

    create function handleNewNotes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom";
 
 import { useState } from "react";
 
+import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
+
 import { Textarea } from "../../components/Textarea";
 import { NewItem } from "../../components/NewItem";
 import { Section } from "../../components/Section";
@@ -12,11 +15,16 @@ import { Input } from "../../components/Input";
 import { Container, Form } from "./styles";
 
 export function New() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState("");
 
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate();
 
   function handleAddLink() {
     setLinks((prevState) => [...prevState, newLink]);
@@ -36,6 +44,18 @@ export function New() {
     setTags((prevState) => prevState.filter((tag) => tag !== deleted));
   }
 
+  async function handleNewNote() {
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links,
+    });
+
+    alert("Nota criada com sucesso!");
+    navigate("/");
+  }
+
   return (
     <Container>
       <Header />
@@ -47,8 +67,14 @@ export function New() {
             <Link to="/"> Voltar</Link>
           </header>
 
-          <Input placeholder="Título" />
-          <Textarea placeholder="Observações" />
+          <Input
+            placeholder="Título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Textarea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
           <Section title="Links uteis">
             {links.map((link, index) => (
@@ -89,7 +115,7 @@ export function New() {
             </div>
           </Section>
 
-          <Button title="Salvar" />
+          <Button title="Salvar" onClick={handleNewNote} />
         </Form>
       </main>
     </Container>
